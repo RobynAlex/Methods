8cc5393b281241731953461a782dbb2f8f5f863b
RobynAlex <robyn.alexander1111@gmail.com>
2022-01-13 22:39:38 -0600

    added more lines

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -6,7 +6,19 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            string faveColor = Console.ReadLine();
+
+            Console.WriteLine("What about your favorite animal?");
+            string faveAnimal = Console.ReadLine();
+
+            Console.WriteLine("Who is your favorite band?");
+            string faveBand = Console.ReadLine();
+
+            Console.WriteLine($"One time {name} went to a record store called {faveColor} {faveAnimal} and bought his first {faveBand} record.");
         }
     }
 }
